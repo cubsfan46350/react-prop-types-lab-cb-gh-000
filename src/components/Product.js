46a5a33ae61d7a40956eece2f56eb3ff8b68f187
Zachary Baca <cubsfan.46350@gmail.com>
2019-01-27 07:33:53 +0000

    Automatically backed up by Learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -22,5 +22,5 @@ Product.PropTypes = {
   producer: PropTypes.string,
   hasWatermark: PropTypes.bool,
   color: PropTypes.oneOf(['white', 'eggshell-white', 'salmon']),
-  weight: const weightFunction = 
+  weight: const weightFunction = (props, )
 }
