e1968fc88d1a51146916a3956f609fec39ac7076
Zachary Baca <cubsfan.46350@gmail.com>
2019-01-27 07:18:11 +0000

    Automatically backed up by Learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,3 +1,19 @@
 // Code Product Component Here
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+
+export default class Product extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>name: {this.props.name}</h1>
+        <h1>producer: {this.props.producer}</h1>
+        <h1>hasWatermark: {this.props.hasWatermark}</h1>
+        <h1>color: {this.props.color}</h1>
+        <h1>weight: {this.props.weight}</h1>
+      </div>
+      );
+  }
+}
+
